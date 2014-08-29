16193925501c3828d0aea5dca5bad44e4c87e32c
MaxwellPayne <mpmclenn@indiana.edu>
2014-08-29 10:18:26 -0400

    Oops, previously committed the 2013 first assignment. Getting rid of it now.

--- hw1/drought.py
+++
@@ -1,4 +0,0 @@
-from utilities import *
-
-def create_drought(flooded_list, color_of_tile):
-    pass
--- hw1/flood.py
+++
@@ -1,6 +0,0 @@
-from utilities import *
-
-# The flood function should add same-color regions to the flooded_list
-# that are adjacent to the tiles in the flooded list.
-def flood(color_of_tile, flooded_list):
-    pass
