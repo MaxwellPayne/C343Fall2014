42694a00dfd9baa98fe8ba01ac4dceb25f463f5a
MaxwellPayne <mpmclenn@indiana.edu>
2014-08-29 10:12:24 -0400

    Initial commit of all the sample code.

---
+++ hw1/flood.py
@@ -0,0 +1,6 @@
+from utilities import *
+
+# The flood function should add same-color regions to the flooded_list
+# that are adjacent to the tiles in the flooded list.
+def flood(color_of_tile, flooded_list):
+    pass
