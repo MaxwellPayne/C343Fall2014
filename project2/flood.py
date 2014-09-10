115834fa423a6d25394a63b05125e57be89332f3
Max McLennan <mpmclenn@indiana.edu>
2014-09-10 17:10:39 -0400

    Ran 'python floodit.py gentests' which magically wrote good test data into the tests_* files. After running that command. 'python floodit.py test' passed with flying colors. Also ran 'python floodit.py time' which gave me a csv. Started work on an excel file that visually illustrates the Big O of my flood function.

--- project2/flood.py
+++ project2/flood.py
@@ -1,16 +1,23 @@
+from math import sqrt
+import sys
+
 from utilities import *
 from adjacency import adjacency_list, get_screen_size
 # bring in the adjacency list from the cheat
 
+sys.setrecursionlimit(2000)
+
 _ADJACENCY_LIST = None
+_BOARD_LEN = -1
 
 def flood(color_of_tile, flooded_list, screen_size):
-    global _ADJACENCY_LIST
-    if not _ADJACENCY_LIST:
-        from math import sqrt
-        board_size = sqrt(len(color_of_tile))
-        actual_screen_size = get_screen_size(board_size)
+    board_size = int(sqrt(len(color_of_tile)))
+    actual_screen_size = get_screen_size(board_size)
+    global _ADJACENCY_LIST, _BOARD_LEN
+    if not _ADJACENCY_LIST or _BOARD_LEN != board_size:
+        _BOARD_LEN = board_size
         _ADJACENCY_LIST = adjacency_list(actual_screen_size, STEP_SIZE)
+        #print _ADJACENCY_LIST
 
     region_color = color_of_tile[flooded_list[0]]
     # the region color will be color of origin
