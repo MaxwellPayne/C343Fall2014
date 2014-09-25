8e3557589083ca58af02a67b43535337169c1afc
Max McLennan <mpmclenn@indiana.edu>
2014-09-25 17:29:42 -0400

    Added the rotate left and right methods onto avl.py. Think they work but avl_tests.py is misleading right now.

--- project3/avl.py
+++ project3/avl.py
@@ -10,6 +10,38 @@ def less_than(x,y):
 
 class AVLTree(BinarySearchTree):
 
+
+    def rotate_right(self, old_top):
+        switch_branch = old_top.left.right
+        new_top = old_top.left
+        parent_above = old_top.parent
+        
+        if not parent_above: # old_top is root
+            self.root = new_top
+        elif old_top is parent_above.right:
+            parent_above.right = new_top
+        else: # old_top is old_top.parent.left
+            parent_above.left = new_top
+            
+        new_top.right = old_top
+        old_top.left = switch_branch
+
+    def rotate_left(self, old_top):
+        switch_branch = old_top.right.left
+        new_top = old_top.right
+        parent_above = old_top.parent
+
+        if not parent_above: # old_top is root
+            self.root = new_top
+        elif old_top is parent_above.right:
+            parent_above.right = new_top
+        else: # old_top is old_top.parent.left
+            parent_above.left = new_top
+
+        new_top.left = old_top
+        old_top.right = switch_branch
+
+
     # takes value, returns node with key value
     def insert(self, k):
         pass
