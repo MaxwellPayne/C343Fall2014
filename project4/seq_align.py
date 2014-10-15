a1907d6b484dc486ed0dcda1da74596a1ceb191b
Max McLennan <mpmclenn@indiana.edu>
2014-10-15 17:52:59 -0400

    Added the project 4. AlignmentCell seemed like a good class, but we should abandon it because it's overkill. Instead use the simple Cell class within seq_align.py.

---
+++ project4/seq_align.py
@@ -0,0 +1,141 @@
+#! /usr/bin/env python
+
+import sys, time, random
+import pygame
+
+from AlignmentCell import ParentDirection
+
+e_aplh = "abcdefghijklmnopqrstuvwxyz"
+dna_alph = "ACGT"
+
+# generate random string drawn from the given alphabet and of a given length
+def gen_random_string(alphabet, length):
+    a_len = len(alphabet)
+    ret = ""
+    for n in range(length):
+        ret += alphabet[random.randint(0, a_len-1)]
+    return ret
+
+# print gen_random_string(e_aplh, 5)
+
+SPACE_CHAR = '_'
+SPACE_PENALTY = -1
+
+# the scoring function
+def s(x, y):
+    if x == SPACE_CHAR or y == SPACE_CHAR:
+        return SPACE_PENALTY
+    elif x == y:
+        return 2
+    else:
+        return -2
+
+TILE_SIZE = 40
+tile_color = (255, 255, 255)
+highlight_color = (120, 129, 250)
+
+def init_board(m, n):
+    screen = pygame.display.set_mode(((m+2)*TILE_SIZE, (n+2)*TILE_SIZE))
+    screen.fill((0, 0, 0))
+    pygame.display.set_caption('Dot Board')
+    pygame.font.init()
+    font = pygame.font.Font(None, 15)
+    return screen, font
+
+def create_tile(font, text, color):
+    tile = pygame.Surface((TILE_SIZE, TILE_SIZE))
+    tile.fill(color)
+    b1 = font.render(text, 1, (0, 0, 0))
+    tile.blit(b1, (TILE_SIZE/2, TILE_SIZE/2))
+    return tile
+
+def render_board(board, font, s1, s2, F):
+    for i in range(len(s1)):
+        tile = create_tile(font, s1[i], tile_color)
+        board.blit(tile, ((i+2)*TILE_SIZE, 0))
+    tile = create_tile(font, '', tile_color); board.blit(tile, (0, 0))
+    tile = create_tile(font, '', tile_color); board.blit(tile, (TILE_SIZE, 0))
+    for j in range(len(s2)):
+        tile = create_tile(font, s2[j], tile_color)
+        board.blit(tile, (0, (j+2)*TILE_SIZE))
+    tile = create_tile(font, '', tile_color); board.blit(tile, (0, TILE_SIZE))
+    for (x,y) in sorted(F.keys()):
+        tile = create_tile(font, str(F[(x,y)]), tile_color)
+        board.blit(tile, ((x+1)*TILE_SIZE, (y+1)*TILE_SIZE))
+    
+
+class Cell:
+    def __init__(self, val, direction):
+        self.val, self.direction = val, direction
+
+    def __repr__(self):
+        return '<Cell val %s direction %s>' % (self.val, self.direction)
+
+def seq_align(s1, s2, enable_graphics=True):
+    strX, strY = "AC", "CA"
+    matrix = [[None for _ in range(len(strX) + 1)] for _ in range(len(strY) + 1)]
+
+    for r, row in enumerate(matrix):
+        for c, col in enumerate(matrix):
+            if r == 0 and c == 0:
+                # top left corner
+                matrix[r][c] = Cell(0, None)
+            elif r == 0:
+                matrix[r][c] = Cell(-c, ParentDirection.LEFT)
+            elif c == 0:
+                matrix[r][c] = Cell(-r, ParentDirection.UP)
+            else: # evaluate
+                up, left, diag = matrix[r-1][c], matrix[r][c-1], matrix[r-1][c-1]
+                
+
+
+    """for j, row in enumerate(matrix):
+        strrow = ''
+        for i, col in enumerate(row):
+            cell = AlignmentCell(i, j, strX, strY, s, matrix, parentDirection=)
+            matrix[j][i] = cell
+            print "I am %s and my parent is %s" % (cell, cell.parent)
+            strrow += str(( ("_" + strX)[i], ("_" + strY)[j]))
+        #print strrow"""
+    for r in matrix:
+        print r
+    
+
+"""
+if len(sys.argv) == 2 and sys.argv[1] == 'test':
+    f=open('tests.txt', 'r');tests= eval(f.read());f.close()
+    cnt = 0; passed = True
+    for ((s1, s2), (a1, a2)) in tests:
+        (ret_a1, ret_a2) = seq_align(s1, s2, False)
+        if (ret_a1 != a1) or (ret_a2 != a2):
+            print("test#" + str(cnt) + " failed...")
+            passed = False
+        cnt += 1
+    if passed: print("All tests passed!")
+elif len(sys.argv) == 2 and sys.argv[1] == 'gentests':
+    tests = []
+    for n in range(25):
+        m = random.randint(8, 70); n = random.randint(8, 70)
+        (s1, s2) = (gen_random_string(dna_alph, m), gen_random_string(dna_alph, n))
+        (a1, a2) = seq_align(s1, s2, False)
+        tests.append(((s1, s2), (a1, a2)))
+    f=open('tests.txt', 'w');f.write(str(tests));f.close()
+else:
+    l = [('ACACACTA', 'AGCACACA'), ('IMISSMISSISSIPI', 'MYMISSISAHIPPIE')]
+    enable_graphics = True
+    if enable_graphics: pygame.init()
+    for (s1, s2) in l:
+        print 'sequences:'
+        print (s1, s2)
+        
+        m = len(s1)
+        n = len(s2)
+        
+        print 'alignment: '
+        print seq_align(s1, s2, enable_graphics)
+    
+    if enable_graphics: pygame.quit()
+"""
+
+if __name__ == '__main__':
+    seq_align(2,2)
