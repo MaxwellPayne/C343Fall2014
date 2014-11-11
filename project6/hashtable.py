c27bca989feee266532f2c72ab0c3879e78db0be
Max McLennan <mpmclenn@indiana.edu>
2014-11-11 16:27:43 -0500

    Started project6. Got a start on Hashtable class. Able to initialize the array, insert keys, and get keys. Does not yet handle collisions at all; simply throws exception when it attempts to insert a colliding key

---
+++ project6/hashtable.py
@@ -0,0 +1,90 @@
+
+class Hashtable(object):
+    doubling_factor = 0.5
+
+    def __init__(self, dict):
+        self.n, self.m = 0, len(dict) * 4
+        self.array = [None for _ in xrange(self.m)]
+        self._keys = set(dict.keys())
+        for k, v in dict.iteritems():
+            self[k] = v
+
+    @property
+    def cls(self):
+        """convenience method that returns
+        the class of self"""
+        return self.__class__
+
+    @property
+    def load_factor(self):
+        return self.n / float(self.m)
+
+    def _hash_key(self, key):
+        return hash(key) % self.m
+
+    def _double(self):
+        print 'doubling!'
+        old_array = self.array
+        old_hashkeys = map(lambda k: self._hash_key(k), self.keys())
+        # cloning keys this way does not handle collisions
+
+        # double m, create a doubled array
+        self.m *= 2
+        self.array = [None for _ in xrange(self.m)]
+        
+        for key, old_hashkey in (self.keys(), old_hashkeys):
+            # copy all the (key, value) pairs over to new array
+            self.array[self._hash_key] = old_array[old_hashkey]
+
+
+    def __getitem__(self, key):
+        hashkey = self._hash_key(key)
+        if self.array[hashkey] is None:
+            raise KeyError('key %s is not in hashtable' % str(key))
+        return self.array[self._hash_key(key)]
+
+    def __setitem__(self, key, value):
+        # does not handle collisions!
+        idx = self._hash_key(key)
+        
+        # throws exception on collision b/c haven't handled it yet
+        if self.array[idx] is not None: raise Exception('collision!')
+        self.array[idx] = value
+        self.n += 1
+        self._keys.add(key)
+
+        if self.load_factor >= self.cls.doubling_factor:
+            self._double()
+
+    def __delitem__(self, key):
+        # {insert delete code here}
+        self.n -= 1
+        self._keys.remove(key)
+
+    def keys(self):
+        return tuple(self._keys)
+
+    def __str__(self):
+        return str(self.array)
+
+
+if __name__ == '__main__':
+    import random
+
+    INIT_KEY_COUNT = 10 # number of keys initially inserted
+    ks = tuple((random.randrange(10000) for _ in xrange(INIT_KEY_COUNT)))
+    vs = tuple((random.randrange(10000) for _ in xrange(INIT_KEY_COUNT)))
+    d = {}
+    for k, v in zip(ks, vs):
+        d[k] = v
+
+    h = Hashtable(d)
+
+    # filling the Hashtable with these extra keys will
+    # force it to hit the doubling threshold
+    ks2 = tuple((random.randrange(10000) for _ in xrange(INIT_KEY_COUNT)))
+    vs2 = tuple((random.randrange(10000) for _ in xrange(INIT_KEY_COUNT)))
+    for k, v in zip(ks2, vs2):
+        h[k] = v
+    
+    print h.keys()
